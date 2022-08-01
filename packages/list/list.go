19073f86ffafac90f18aad20126ad5f92f93eff5
Jiacheng Dong <2334173653@qq.com>
2022-08-01 22:14:48 +0800

    refactor: array methods

--- packages/list/list.go
+++ packages/list/list.go
@@ -1,38 +1,32 @@
 package list
 
-type List struct {
-	arr []int
-}
+type List []int
 
 func New(arr []int) *List {
-	ins := &List{arr: arr}
-	return ins
+	ins := List(arr)
+	return &ins
 }
 
 func (l *List) ForEach(fn func(int, int)) {
-	for index, item := range l.arr {
+	for index, item := range *l {
 		fn(item, index)
 	}
 }
 
 func (l *List) Map(fn func(int, int) int) *List {
 	res := make([]int, 0)
-	for index, item := range l.arr {
+	for index, item := range *l {
 		res = append(res, fn(item, index))
 	}
-	return &List{arr: res}
+	return New(res)
 }
 
 func (l *List) Filter(fn func(int, int) bool) *List {
 	res := make([]int, 0)
-	for index, item := range l.arr {
+	for index, item := range *l {
 		if fn(item, index) {
 			res = append(res, item)
 		}
 	}
-	return &List{arr: res}
-}
-
-func (l *List) ToPrimitive() []int {
-	return l.arr
+	return New(res)
 }
